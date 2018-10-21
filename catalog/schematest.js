028297ccfeebc46d23db224c5a279aec3d164e5e
Chirag Rathod <chirag@chirag-rathod>
2018-10-21 15:50:16 +0530

    Deleted test file

--- catalog/schematest.js
+++
@@ -1,5 +0,0 @@
-var Validator = require('jsonschema').Validator;
-  var v = new Validator();
-  var instance = {a:"4"};
-  var schema = {"type": "object"};
-  console.log(v.validate(instance, schema).errors.length);
