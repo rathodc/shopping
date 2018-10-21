d88e5169934680054f15f0eacd01a2d54b8f2846
Chirag Rathod <chirag@chirag-rathod>
2018-10-21 15:26:43 +0530

    Fixed an issue where allowed token was giving authorization error

--- catalog/ml/catalog.js
+++ catalog/ml/catalog.js
@@ -1,12 +1,13 @@
 var load_manager = require(process.env.NODE_SRC_DIR+"/load_manager");
 var log = require('log4js').getLogger("catalog");
+var allowed_tokens = ["e4b76ae67e4b", "604605a6bfe0", "22437a5dc2f5"]
 
 var define_middleware = function(resolve,reject){
 	router.use('/catalog',function(req,res,next){
 		log.debug("In ml | Catalog Middleware ")
 		log.debug("req.headers.Auth-Token "+req.headers["Auth-Token"])
 		log.debug(req.headers["auth-token"])
-		if(!req.headers["auth-token"]){
+		if(!req.headers["auth-token"] || allowed_tokens.indexOf(req.headers["auth-token"])==-1){
 			res.setHeader('Content-Type', 'application/json')
 			res.status(401)
 			res.send({
